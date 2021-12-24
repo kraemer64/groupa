97c3d7006bcc77310618e32cb025f9b199292c14
kraemer64 <54526557+kraemer64@users.noreply.github.com>
2021-12-24 18:43:37 +0100

    Update settings.py
    Including the app to the project by adding a reference to its configuration

--- poll_app/mysite/settings.py
+++ poll_app/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig'
     'django.contrib.admin',         #Adminsite
     'django.contrib.auth',          #authentication system
     'django.contrib.contenttypes',  #framework for content types
