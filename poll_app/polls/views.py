df840f04f860fccdf40e0419553203bdb4427ae4
kraemer64 <54526557+kraemer64@users.noreply.github.com>
2022-01-03 22:18:46 +0100

    Added function to vote view
    Added vote view.

--- poll_app/polls/views.py
+++ poll_app/polls/views.py
@@ -14,6 +14,7 @@ def index(request):
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
+    print(question.)
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
@@ -21,4 +22,15 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice."
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
