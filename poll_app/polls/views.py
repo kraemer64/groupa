01e6b423b3686c8f7ce7845ae725c1e65e8e8d55
kraemer64 <54526557+kraemer64@users.noreply.github.com>
2022-01-04 23:02:54 +0100

    Update views.py
    Added new imports.
    Created new class IndexView for amend views.

--- poll_app/polls/views.py
+++ poll_app/polls/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
-from django.template import loader
-from django.http import Http404
-from .models import Question, Choice
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+
+from .models import Choice, Question
 
 # Create your views here.
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
