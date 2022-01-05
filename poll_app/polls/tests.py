e45ebf197b12d681c0b811f618564d0d163b58e8
kraemer64 <54526557+kraemer64@users.noreply.github.com>
2022-01-05 23:29:36 +0100

    Update tests.py
    Added indent

--- poll_app/polls/tests.py
+++ poll_app/polls/tests.py
@@ -12,4 +12,4 @@ class QuestionModelsTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
 
-self.assertIs(future_question.was_published_recently(), False)
+        self.assertIs(future_question.was_published_recently(), False)
