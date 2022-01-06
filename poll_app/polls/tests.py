d8c673f8e0621632064598051fe8a0d90382340e
kraemer64 <54526557+kraemer64@users.noreply.github.com>
2022-01-06 23:47:07 +0100

    Update tests.py
    Added test_two_past_questions function.

--- poll_app/polls/tests.py
+++ poll_app/polls/tests.py
@@ -66,4 +66,11 @@ class QuestionIndexViewTests(TestCase):
             [question],
         )
 
-    
+    def test_two_past_questions(self):
+        question1 = create_question(question_text="Past question 1.", days=-30)
+        question2 = create_question(question_text="Past question 2.", days=-5)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            [question2, question1],
+        )
