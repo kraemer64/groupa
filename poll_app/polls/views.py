674617074e505c58f5845e75092dfe8fbdf4d4f7
kraemer64 <54526557+kraemer64@users.noreply.github.com>
2022-01-06 22:52:45 +0100

    Update views.py
    Added timezone module.

--- poll_app/polls/views.py
+++ poll_app/polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
 
