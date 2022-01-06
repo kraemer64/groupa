d7aa37ab96a1368e7eeb2aa0207b28558356588e
kraemer64 <54526557+kraemer64@users.noreply.github.com>
2022-01-06 23:02:35 +0100

    Update views.py
    Changed get_queryset function to not pusblish polls with future time.

--- poll_app/polls/views.py
+++ poll_app/polls/views.py
@@ -14,7 +14,8 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by(
+            '-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
