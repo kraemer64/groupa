c7d975ebec8d1f43c9851b7617b4fc606c91f787
kraemer64 <54526557+kraemer64@users.noreply.github.com>
2022-01-13 21:21:19 +0100

    Model changes
    New sql migration to fix typo. Added choice description charfield.

--- poll_app/polls/models.py
+++ poll_app/polls/models.py
@@ -21,8 +21,8 @@ class Question(models.Model):
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
-    choice_desc = models.IntegerField(default=0)
+    choice_desc = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.question_text
+        return self.choice_text
